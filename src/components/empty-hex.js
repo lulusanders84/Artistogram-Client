6ba42b5180c35aa7c8a7c2c62a5a9d9f6215a623
Lucy Sanders <lucyhsanders@gmail.com>
2018-12-18 17:41:38 -0700

    added artistogramPage tests

--- src/components/empty-hex.js
+++ src/components/empty-hex.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import ArtistIcon from './artist-icon';
+import ArtistogramIcon from './artistogram-icon';
 import uuid from 'uuid/v4';
 
 export function addEmptyHex(artistIcons) {
   for(let i=0; i < artistIcons.length; i++) {
-    let emptyHex = <ArtistIcon imageUrl='' key={uuid()} />;
+    let emptyHex = <ArtistogramIcon imageUrl='' key={uuid()} />;
     if (i % 8 == 0) {
       artistIcons.splice(i, 0, emptyHex);
     }
---
+++ src/tests/artistogram-page.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+
+import { ArtistogramPage } from '../components/artistogram-page';
+import { saveDestination } from '../actions';
+
+it('Renders without crashing', () => {
+  const dispatch = jest.fn();
+    shallow(<ArtistogramPage focalArtist={{name: 'oasis'}} dispatch={dispatch}/>);
+});
+
+it('Dispatches saveDestination from ArtistogramPage', () => {
+        const dispatch = jest.fn();
+        const wrapper = shallow(
+            <ArtistogramPage focalArtist='oasis' dispatch={dispatch} />
+        );
+        const instance = wrapper.instance();
+        instance.componentDidMount();
+        expect(dispatch).toHaveBeenCalledWith(saveDestination('/artistogram'));
+    });
